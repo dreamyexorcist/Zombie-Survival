c935d728546b2212b1cac78a7a5b1d8c848dc499
Olive <anna.olivia.heart@gmail.com>
2022-03-22 02:01:07 +0000

    Menus & Terrain

--- Olivia_ZombieSurvival/Olivia_ZombieSurvival/Assets/00_MyAssets/Scripts/UiHandler.cs
+++ Olivia_ZombieSurvival/Olivia_ZombieSurvival/Assets/00_MyAssets/Scripts/UiHandler.cs
@@ -7,16 +7,26 @@ public class UiHandler : MonoBehaviour
 {
     public void loadOpeningScene()  
     {
-
         SceneManager.LoadScene(1);  
-
     }
+
+    public void loadGameScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void loadSettingsScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+   /* public void loadMenuScene()
+    {
+        SceneManager.LoadScene(1);
+    }*/
 
     public void QuitGame()
     {
-
         Application.Quit(); 
-
     }
 
 }

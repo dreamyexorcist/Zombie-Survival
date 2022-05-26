bb85d319a1c351bf97983be8e695748666f2e914
Olive <anna.olivia.heart@gmail.com>
2022-05-26 02:56:46 +0100

    Level Design
    Load Level 2

---
+++ Olivia_ZombieSurvival/Olivia_ZombieSurvival/Assets/00_MyAssets/Scripts/LoadLevel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+   
+    [SerializeField] private string loadLevel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(3); // loads scene When player enter the trigger collider
+        }
+    }
+   
+}

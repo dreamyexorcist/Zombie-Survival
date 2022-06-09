b17106f70a283bc20a9e8e44bde12078718694dd
Olive <anna.olivia.heart@gmail.com>
2022-06-09 04:45:58 +0100

    Settings Menu

---
+++ Olivia_ZombieSurvival/Olivia_ZombieSurvival/Assets/00_MyAssets/Scripts/UI/GameSettings.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameSettings
+{
+    public bool fullscreen;
+    public int textureQuality;
+    public int antialiasing;
+    public int vSync;
+    public int resolutionIndex;
+    public float musicVolume;
+
+}
---
+++ Olivia_ZombieSurvival/Olivia_ZombieSurvival/Assets/00_MyAssets/Scripts/UI/SettingsManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class SettingsManager : MonoBehaviour
+{
+    public Toggle fullscreenToggle;
+    public Dropdown resolutionDropdown;
+    public Dropdown textureQualityDropdown;
+    public Dropdown antialiasingDropdown;
+    public Dropdown vSybcDropdown;
+    public Slider musicVolumeSlider;
+    public Button applybutton;
+
+    public AudioSource musicSource;
+    public Resolution[] resolutions;
+    public GameSettings gameSettings;
+
+    void OnEnable()
+    {
+        gameSettings = new GameSettings();
+
+        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResultionChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        antialiasingDropdown.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
+        vSybcDropdown.onValueChanged.AddListener(delegate { OnVSyncChange(); });
+        musicVolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+        applybutton.onClick.AddListener(delegate { OnApplyButtonClick(); });
+
+        resolutions = Screen.resolutions;
+        foreach (Resolution resolution in resolutions)
+        {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+
+        LoadSettings();
+
+    }
+    public void OnFullscreenToggle()
+    {
+        gameSettings.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+    }
+    public void OnResultionChange()
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+        gameSettings.resolutionIndex = resolutionDropdown.value;
+    }
+    public void OnTextureQualityChange()
+    {
+        QualitySettings.masterTextureLimit = gameSettings.textureQuality = textureQualityDropdown.value;        
+    }
+    public void OnAntialiasingChange()
+    {
+        QualitySettings.antiAliasing = gameSettings.antialiasing = (int)Mathf.Pow(2f, antialiasingDropdown.value);
+    }
+    public void OnVSyncChange()
+    {
+        QualitySettings.vSyncCount = gameSettings.vSync = vSybcDropdown.value;
+    }
+    public void OnMusicVolumeChange()
+    {
+        musicSource.volume = gameSettings.musicVolume = musicVolumeSlider.value;
+    }
+    public void OnApplyButtonClick()
+    {
+        SaveSettings();
+    }
+    public void SaveSettings()
+    {
+        string jsonData = JsonUtility.ToJson(gameSettings);
+        File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
+    }
+    public void LoadSettings()
+    {
+        gameSettings = JsonUtility.FromJson<GameSettings>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
+
+        musicVolumeSlider.value = gameSettings.musicVolume;
+        antialiasingDropdown.value = gameSettings.antialiasing;
+        vSybcDropdown.value = gameSettings.vSync;
+        textureQualityDropdown.value = gameSettings.textureQuality;
+        resolutionDropdown.value = gameSettings.resolutionIndex;
+        fullscreenToggle.isOn = gameSettings.fullscreen;
+
+        resolutionDropdown.RefreshShownValue();
+    }
+}
